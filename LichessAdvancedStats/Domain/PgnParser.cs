0602ba7a5d8ac2561f841deb54fb865092397ee3
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-06-13 23:47:51 +0300

    PgnParserAdded

---
+++ LichessAdvancedStats/Domain/PgnParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LichessAdvancedStats.Model;
+
+namespace LichessAdvancedStats.Domain
+{
+    public class PgnParser
+    {
+        public List<Game> Parse(string pgn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
