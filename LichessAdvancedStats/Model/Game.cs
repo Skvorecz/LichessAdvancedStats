c7ac0d2dee72e1f290b11c3ba89b27527d84d51b
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-06-13 23:43:21 +0300

    Game, Move added

---
+++ LichessAdvancedStats/Model/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LichessAdvancedStats.Model
+{
+    public class Game
+    {
+        public Dictionary<string, string> Attributes;
+        public List<Move> Moves { get; set; }
+
+        public Game()
+        {
+            Attributes = new Dictionary<string, string>();
+            Moves = new List<Move>();
+        }
+    }
+}
---
+++ LichessAdvancedStats/Model/Move.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LichessAdvancedStats.Model
+{
+    public class Move
+    {
+        public string WhiteMove { get; set; }
+        public string BlackMove { get; set; }
+    }
+}
