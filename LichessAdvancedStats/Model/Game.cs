22acea9a9613ee73f81c3a492080d9e0ec36de91
Andrey_Skvorcov <Skvorec-2010@mail.ru>
2021-06-14 13:33:41 +0300

    Game and Move Equals() realization

--- LichessAdvancedStats/Model/Game.cs
+++ LichessAdvancedStats/Model/Game.cs
@@ -15,5 +15,50 @@ namespace LichessAdvancedStats.Model
             Attributes = new Dictionary<string, string>();
             Moves = new List<Move>();
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var game = (Game)obj;
+            
+            if(game.Moves.Count != this.Moves.Count)
+            {
+                return false;
+            }
+
+            for(var i = 0; i < game.Moves.Count; i++)
+            {
+                if(game.Moves[i].Equals(this.Moves[i]) == false)
+                {
+                    return false;
+                }
+            }
+
+
+            if(game.Attributes.Count != this.Attributes.Count)
+            {
+                return false;
+            }
+
+            foreach(var pair in game.Attributes)
+            {
+                if(!this.Attributes.ContainsKey(pair.Key)
+                    || this.Attributes[pair.Key] != pair.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {            
+            return base.GetHashCode();
+        }
     }
 }
